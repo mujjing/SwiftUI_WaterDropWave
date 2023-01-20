1cc9cd58a2bf61bef240c5fa68fa0aebc468abbe
전지훈 <junji636@gmail.com>
2023-01-20 23:55:38 +0900

    물방울 모양 추가

--- SwiftUI_WaterDropWave/SwiftUI_WaterDropWave/Home.swift
+++ SwiftUI_WaterDropWave/SwiftUI_WaterDropWave/Home.swift
@@ -12,6 +12,28 @@ struct Home: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 100, height: 100)
                 .clipShape(Circle())
+                .padding(10)
+                .background(.white, in: Circle())
+            
+            Text("죠르디")
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+                .padding(.bottom, 30)
+            
+            GeometryReader { geo in
+                let size = geo.size
+                
+                ZStack {
+                    //MARK: Water Drop
+                    Image(systemName: "drop.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.white)
+                }
+                .frame(width: size.width, height: size.height, alignment: .center)
+            }
+            .frame(height: 350)
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
