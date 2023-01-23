9ca7360e8ae086b62195ef4641e2c58138359e80
전지훈 <junji636@gmail.com>
2023-01-23 22:23:20 +0900

    이미지 안에 물결 표시 추가

--- SwiftUI_WaterDropWave/SwiftUI_WaterDropWave/Home.swift
+++ SwiftUI_WaterDropWave/SwiftUI_WaterDropWave/Home.swift
@@ -30,6 +30,16 @@ struct Home: View {
                         .renderingMode(.template)
                         .aspectRatio(contentMode: .fit)
                         .foregroundColor(.white)
+                    // Streching in X Axis
+                        .scaleEffect(x: 1.1, y: 1)
+                    // Wave Form Shape
+                    WaterWave(progress: 0.5, waveHeight: 0.1, offset: size.width)
+                    // Masking into Drop Shape
+                        .mask {
+                            Image(systemName: "drop.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
                 }
                 .frame(width: size.width, height: size.height, alignment: .center)
             }
@@ -44,5 +54,40 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+struct WaterWave: Shape {
+    
+    var progress: CGFloat
+    // Wave Height
+    var waveHeight: CGFloat
+    // Initial Animation Start
+    var offset: CGFloat
+    
+    // Enabling Animation
+    var animatableData: CGFloat {
+        get{offset}
+        set{offset = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: .zero)
+            //MARK: Drawing Waves using Sine
+            let progressHeight: CGFloat = (1 - progress) * rect.height
+            let height = waveHeight * rect.height
+            
+            for value in stride(from: 0, to: rect.width, by: 2) {
+                let x: CGFloat = value
+                let sine: CGFloat = sin(Angle(degrees: value + offset).radians)
+                let y: CGFloat = progressHeight + (height * sine)
+
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+            
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+        }
     }
 }
